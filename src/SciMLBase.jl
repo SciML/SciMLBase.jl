2f93a17a50035094fe91448088d3b427291a2f61
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-01-25 22:45:36 -0500

    Split SciMLBase from DiffEqBase
    There are many reasons for this. 
    
    1. DiffEqBase.jl has grown to be a base between the differential equation solvers, with common tools for nonlinear solvers and callbacks adding a ton of dependencies. Users like @mauro3 noticed this in https://github.com/SciML/DiffEqBase.jl/issues/618
    
    2. A lot of DiffEqBase was becoming unrelated to differential equations. OptimizationProblem, NonlinearProblem, all of those functions, etc. It's a full interface for SciML, not DiffEq which is by now a subset of the full ecosystem. But it needed to play the Base role, which meant that nonlinear problems were defined in the repo carrying all of the dependencies for the ODE solver's callback system.
    
    3. It started inviting circular dependencies. Some of these problems are pervasive. We need to get the simplest form to GalacticOptim.jl, Quadrature.jl, NonlinearSolve.jl, etc. which are becoming the DiffEq's of their respective regimes (though there's not as much to do in those areas). But this meant NonlinearSolve.jl needed NonlinearProblem, but NonlinearSolve.jl was used in the callbacks, so... it had its own problem type. This gives an issue to a system like MTK which then has no unique type to target.
    
    4. There's a lot of churn on the DiffEq solver portions of DiffEqBase, but relatively little on the actual interface portions. So it makes sense to let them be more stable.
    
    5. There's a ton of packages which are @requires in DiffEqBase, which adds to the compile time. This was done because the library was supposed to avoid dependencies, but as the central piece of the differential equation solvers it really couldn't, and so it used required to fake lower dependencies but some of those like ForwardDiff.jl should really be promoted to being a real dependency now that it doesn't have the "idea" of being no-dependency.
    
    For backwards compatibility, DiffEqBase reexports SciMLBase and imports a ton of the abstract types it used to have. This should allow for the migration to take place over time, while still fixing some of the MTK and NonlinearSolve issues that need a fix ASAP.
    
    DiffEqBase will have a companion PR to match it.
    
    There are some remaining issues here though:
    
    1. I would like to get rid of the LabelledArrays dependency. Is there a good generic to use for the names? `nameof`? 
    
    2. Is the split at the right level?
    
    3. I want to drop the Queryverse stuff like TableTraits and IteratorInterfaceExtensions since we have Tables.jl

--- src/SciMLBase.jl
+++ src/SciMLBase.jl
@@ -1,5 +1,553 @@
 module SciMLBase
 
-# Write your package code here.
+using RecipesBase, RecursiveArrayTools, Tables, TableTraits,
+      IteratorInterfaceExtensions, TreeViews
+using DocStringExtensions
+import LabelledArrays
+import Logging
+
+abstract type SciMLProblem end
+
+# Problems
+"""
+$(TYPEDEF)
+
+Base type for all DifferentialEquations.jl problems. Concrete subtypes of
+`DEProblem` contain the necessary information to fully define a differential
+equation of the corresponding type.
+"""
+abstract type DEProblem <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DEElement end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DESensitivity end
+
+"""
+$(TYPEDEF)
+
+Base for types which define linear systems.
+"""
+abstract type AbstractLinearProblem{bType,isinplace} <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define nonlinear systems.
+"""
+abstract type AbstractNonlinearProblem{uType,isinplace} <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define integrals suitable for quadrature.
+"""
+abstract type AbstractQuadratureProblem{isinplace} <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define equations for optimization
+"""
+abstract type AbstractOptimizationProblem{isinplace} <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define steady state problems for ODE systems.
+"""
+abstract type AbstractSteadyStateProblem{uType,isinplace} <: DEProblem end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractNoiseProblem <: DEProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define ODE problems.
+"""
+abstract type AbstractODEProblem{uType,tType,isinplace} <: DEProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define discrete problems.
+"""
+abstract type AbstractDiscreteProblem{uType,tType,isinplace} <:
+                      AbstractODEProblem{uType,tType,isinplace} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractAnalyticalProblem{uType,tType,isinplace} <:
+                      AbstractODEProblem{uType,tType,isinplace} end
+
+"""
+$(TYPEDEF)
+
+Base for types which define RODE problems.
+"""
+abstract type AbstractRODEProblem{uType,tType,isinplace,ND} <: DEProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define SDE problems.
+"""
+abstract type AbstractSDEProblem{uType,tType,isinplace,ND} <:
+                      AbstractRODEProblem{uType,tType,isinplace,ND} end
+
+"""
+$(TYPEDEF)
+
+Base for types which define DAE problems.
+"""
+abstract type AbstractDAEProblem{uType,duType,tType,isinplace} <: DEProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define DDE problems.
+"""
+abstract type AbstractDDEProblem{uType,tType,lType,isinplace} <: DEProblem end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractConstantLagDDEProblem{uType,tType,lType,isinplace} <:
+                      AbstractDDEProblem{uType,tType,lType,isinplace} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSecondOrderODEProblem{uType,tType,isinplace} <: AbstractODEProblem{uType,tType,isinplace} end
+
+"""
+$(TYPEDEF)
+
+Base for types which define BVP problems.
+"""
+abstract type AbstractBVProblem{uType,tType,isinplace} <: AbstractODEProblem{uType,tType,isinplace} end
+
+"""
+$(TYPEDEF)
+
+Base for types which define jump problems.
+"""
+abstract type AbstractJumpProblem{P,J} <: DEProblem end
+
+"""
+$(TYPEDEF)
+
+Base for types which define SDDE problems.
+"""
+abstract type AbstractSDDEProblem{uType,tType,lType,isinplace,ND} <: DEProblem end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractConstantLagSDDEProblem{uType,tType,lType,isinplace,ND} <:
+                      AbstractSDDEProblem{uType,tType,lType,isinplace,ND} end
+
+"""
+$(TYPEDEF)
+
+Base for types which define PDE problems.
+"""
+abstract type AbstractPDEProblem <: DEProblem end
+
+# Algorithms
+"""
+$(TYPEDEF)
+"""
+abstract type SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DEAlgorithm <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractLinearAlgorithm <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractNonlinearAlgorithm <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractQuadratureAlgorithm <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSteadyStateAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractODEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSecondOrderODEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractRODEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSDEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDAEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDDEAlgorithm <: DEAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSDDEAlgorithm <: DEAlgorithm end
+"""
+$(TYPEDEF)
+"""
+abstract type EnsembleAlgorithm <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSensitivityAlgorithm{CS,AD,FDT} <: SciMLAlgorithm end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DAEInitializationAlgorithm <: SciMLAlgorithm end
+
+# PDE Discretizations
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDiscretization <: SciMLAlgorithm end
+
+# Monte Carlo Simulations
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractEnsembleProblem <: SciMLProblem end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractEnsembleEstimator <: SciMLAlgorithm end
+
+export EnsembleProblem
+export EnsembleSolution, EnsembleTestSolution, EnsembleSummary
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDiffEqInterpolation <: Function end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDEOptions end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DECache end
+
+"""
+$(TYPEDEF)
+"""
+abstract type DECallback end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractContinuousCallback <: DECallback end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDiscreteCallback <: DECallback end
+
+# Integrators
+"""
+$(TYPEDEF)
+"""
+abstract type DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSteadyStateIntegrator{Alg, IIP, U} <: DEIntegrator{Alg, IIP, U, Nothing} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractODEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSecondOrderODEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractRODEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSDEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDDEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDAEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSDDEIntegrator{Alg, IIP, U, T} <: DEIntegrator{Alg, IIP, U, T} end
+
+# Solutions
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractNoTimeSolution{T,N} <: AbstractArray{T,N} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractTimeseriesSolution{T,N,A} <: AbstractDiffEqArray{T,N,A} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractEnsembleSolution{T,N,A} <: AbstractVectorOfArray{T,N,A} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractNoiseProcess{T,N,A,isinplace} <: AbstractDiffEqArray{T,N,A} end
+
+"""
+Union of all base solution types.
+"""
+const SciMLSolution = Union{AbstractTimeseriesSolution,
+                         AbstractNoTimeSolution,
+                         AbstractEnsembleSolution,
+                         AbstractNoiseProcess}
+const DESolution = SciMLSolution
+export SciMLSolution, DESolution
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractLinearSolution{T,N} <: AbstractNoTimeSolution{T,N} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractNonlinearSolution{T,N} <: AbstractNoTimeSolution{T,N} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractQuadratureSolution{T,N} <: AbstractNoTimeSolution{T,N} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSteadyStateSolution{T,N} <: AbstractNoTimeSolution{T,N} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractAnalyticalSolution{T,N,S} <: AbstractTimeseriesSolution{T,N,S} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractODESolution{T,N,S} <: AbstractTimeseriesSolution{T,N,S} end
+
+# Needed for plot recipes
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDDESolution{T,N,S} <: AbstractODESolution{T,N,S} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractRODESolution{T,N,S} <: AbstractODESolution{T,N,S} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDAESolution{T,N,S} <: AbstractODESolution{T,N,S} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSensitivitySolution{T,N,S} <: AbstractTimeseriesSolution{T,N,S} end
+
+# Misc
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractSciMLOperator{T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDiffEqOperator{T} <: AbstractSciMLOperator{T} end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractDiffEqLinearOperator{T} <: AbstractDiffEqOperator{T} end
+
+"""
+$(TYPEDEF)
+
+Base for types defining SciML functions.
+"""
+abstract type AbstractSciMLFunction{iip} <: Function end
+
+"""
+$(TYPEDEF)
+
+Base for types defining differential equation functions.
+"""
+abstract type AbstractDiffEqFunction{iip} <: AbstractSciMLFunction{iip} end
+
+"""
+$(TYPEDEF)
+
+Base for types defining optimization functions.
+"""
+abstract type AbstractOptimizationFunction end
+
+"""
+$(TYPEDEF)
+
+Base for types which define the history of a delay differential equation.
+"""
+abstract type AbstractHistoryFunction <: Function end
+
+"""
+$(TYPEDEF)
+"""
+abstract type AbstractReactionNetwork <: Function end
+
+"""
+$(TYPEDEF)
+
+Base type for AD choices.
+"""
+abstract type AbstractADType end
+
+include("utils.jl")
+include("function_wrappers.jl")
+include("scimlfunctions.jl")
+include("problems/problem_utils.jl")
+include("problems/discrete_problems.jl")
+include("problems/steady_state_problems.jl")
+include("problems/analytical_problems.jl")
+include("problems/basic_problems.jl")
+include("problems/ode_problems.jl")
+include("problems/rode_problems.jl")
+include("problems/sde_problems.jl")
+include("problems/noise_problems.jl")
+include("problems/bvp_problems.jl")
+include("problems/dae_problems.jl")
+include("problems/dde_problems.jl")
+include("problems/sdde_problems.jl")
+include("problems/pde_problems.jl")
+include("problems/problem_traits.jl")
+
+include("solutions/basic_solutions.jl")
+include("solutions/steady_state_solutions.jl")
+include("solutions/ode_solutions.jl")
+include("solutions/rode_solutions.jl")
+include("solutions/dae_solutions.jl")
+include("solutions/solution_interface.jl")
+
+include("ensemble/ensemble_solutions.jl")
+include("ensemble/ensemble_problems.jl")
+include("ensemble/basic_ensemble_solve.jl")
+include("ensemble/ensemble_analysis.jl")
+
+include("integrator_interface.jl")
+include("tabletraits.jl")
+
+function discretize end
+
+export isinplace
+
+export solve, solve!, init, discretize
+
+export LinearProblem, NonlinearProblem, QuadratureProblem, OptimizationProblem
+
+export DiscreteProblem
+export SteadyStateProblem, SteadyStateSolution
+export NoiseProblem
+export ODEProblem, ODESolution
+export DynamicalODEFunction, DynamicalODEProblem,
+       SecondOrderODEProblem, SplitFunction, SplitODEProblem
+export SplitSDEProblem
+export RODEProblem, RODESolution, SDEProblem
+export DAEProblem, DAESolution
+export DDEProblem
+export SDDEProblem
+export PDEProblem
+export IncrementingODEProblem
+
+export BVProblem, TwoPointBVProblem
+
+export remake
+
+export ODEFunction, DiscreteFunction, SplitFunction, DAEFunction, DDEFunction,
+       SDEFunction, SplitSDEFunction, RODEFunction, SDDEFunction, IncrementingODEFunction, NonlinearFunction
+
+export OptimizationFunction
+
+export EnsembleThreads, EnsembleDistributed, EnsembleSplitThreads, EnsembleSerial
+
+export EnsembleAnalysis
+
+export update_coefficients!, update_coefficients,
+       has_expmv!, has_expmv, has_exp, has_mul, has_mul!, has_ldiv, has_ldiv!
 
 end
