7c3a4f6ffac3de69f2730b2b17f8d7a4a27a8d1f
Utkarsh <rajpututkarsh530@gmail.com>
2021-02-02 01:39:48 +0530

    Minor fixes

--- src/solutions/nonlinear_solutions.jl
+++ src/solutions/nonlinear_solutions.jl
@@ -1,15 +1,15 @@
 """
 $(TYPEDEF)
 """
-struct NonlinearSolution{T,N,uType,R,P,A,O} <: AbstractNonlinearSolution{T,N}
+struct NonlinearSolution{T,N,uType,R,P,A,O,uType2} <: AbstractNonlinearSolution{T,N}
   u::uType
   resid::R
   prob::P
   alg::A
   retcode::Symbol
   original::O
-  left::uType
-  right::uType
+  left::uType2
+  right::uType2
 end
 
 const SteadyStateSolution = NonlinearSolution
@@ -26,16 +26,16 @@ function build_solution(prob::AbstractNonlinearProblem,
   N = length((size(prob.u0)...,))
 
   NonlinearSolution{T,N,typeof(u),typeof(resid),
-                    typeof(prob),typeof(alg),typeof(original)}(
+                    typeof(prob),typeof(alg),typeof(original),typeof(left)}(
                     u,resid,prob,alg,retcode,original,left,right)
 end
 
-function sensitivity_solution(sol::AbstractNonlinearProblem,u)
+function sensitivity_solution(sol::AbstractNonlinearSolution,u)
   T = eltype(eltype(u))
   N = length((size(sol.prob.u0)...,))
 
   NonlinearSolution{T,N,typeof(u),typeof(sol.resid),
                     typeof(sol.prob),typeof(sol.alg),
-                    typeof(sol.original)}(
-                    u,sol.resid,sol.prob,sol.alg,sol.retcode,sol.original)
+                    typeof(sol.original),typeof(sol.left)}(
+                    u,sol.resid,sol.prob,sol.alg,sol.retcode,sol.original,sol.left,sol.right)
 end
