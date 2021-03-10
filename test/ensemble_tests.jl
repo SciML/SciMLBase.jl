a63288b709607e7754b8015199b91a7574fa00be
Samuel Isaacson <isaacsas@users.noreply.github.com>
2021-03-10 09:59:38 -0500

    make test more appropriate

--- test/ensemble_tests.jl
+++ test/ensemble_tests.jl
@@ -4,15 +4,15 @@ EA = SciMLBase.EnsembleAnalysis
 
 # tests for https://github.com/SciML/DifferentialEquations.jl/issues/731
 # make sure integer inputs work
-@test EA.componentwise_mean([1,1,1]) ≈ 1.0
-m,v = EA.componentwise_meanvar([1,2,3]) 
-@test m ≈ 2.0
-@test v ≈ 1.0
-mx,my,C = EA.componentwise_meancov([1,2,3], [3,2,1])
-@test mx ≈ 2.0
-@test my ≈ 2.0
-@test C ≈ -1.0
-mx,my,C = EA.componentwise_weighted_meancov([1,2,3], [3,2,1], [1,2,1])
-@test mx ≈ 2.0
-@test my ≈ 2.0
-@test C ≈ -.8
+@test all(EA.componentwise_mean([[1,1],[2,2],[3,3]]) .≈ [2.0,2.0])
+m,v = EA.componentwise_meanvar([[1,1],[2,2],[3,3]]) 
+@test all(m .≈ 2.0)
+@test all(v .≈ 1.0)
+mx,my,C = EA.componentwise_meancov([[1,1],[2,2],[3,3]], [[3,3],[2,2],[1,1]])
+@test all(mx .≈ 2.0)
+@test all(my .≈ 2.0)
+@test all(C .≈ -1.0)
+mx,my,C = EA.componentwise_weighted_meancov([[1,1],[2,2],[3,3]], [[3,3],[2,2],[1,1]], [[1,1],[2,2],[1,1]])
+@test all(mx .≈ 2.0)
+@test all(my .≈ 2.0)
+@test all(C .≈ -.8)
