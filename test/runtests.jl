819ebd1cf5cf5aa1b4c8fb89761c776cd9665d32
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-03-10 00:39:48 -0500

    Fix zero length ensembles and add a downstream test category
    Fixes https://github.com/SciML/DifferentialEquations.jl/issues/732

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,23 @@ using SciMLBase
 const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = ( Sys.iswindows() && haskey(ENV,"APPVEYOR") )
 
+function activate_downstream_env()
+    Pkg.activate("downstream")
+    Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
+    Pkg.instantiate()
+end
+
 @time begin
-@time @safetestset "Display" begin include("display.jl") end
-@time @safetestset "Existence functions" begin include("existence_functions.jl") end
-@time @safetestset "Integrator interface" begin include("integrator_tests.jl") end
+
+s
+if GROUP == "Core" || GROUP == "All"
+    @time @safetestset "Display" begin include("display.jl") end
+    @time @safetestset "Existence functions" begin include("existence_functions.jl") end
+    @time @safetestset "Integrator interface" begin include("integrator_tests.jl") end
+end
+
+if !is_APPVEYOR && GROUP == "Downstream"
+    activate_downstream_env()
+    @time @safetestset "Ensembles of Zero Length Solutions" begin include("downstream/ensemble_zero_length.jl") end
+end
 end
