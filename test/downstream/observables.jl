a43dfb73ec66c31b2fe3f91d61eb4df5fb26d97b
Sharan Yalburgi <sharanyalburgi@gmail.com>
2021-06-21 17:13:26 +0530

    Revert back to old example for downstream tests

--- test/downstream/observables.jl
+++ test/downstream/observables.jl
@@ -1,6 +1,5 @@
 using ModelingToolkit, OrdinaryDiffEq, Test
 
-#= fails
 @parameters t σ ρ β
 @variables x(t) y(t) z(t)
 D = Differential(t)
@@ -12,12 +11,12 @@ eqs = [D(x) ~ σ*(y-x),
 lorenz1 = ODESystem(eqs,name=:lorenz1)
 lorenz2 = ODESystem(eqs,name=:lorenz2)
 
-@variables a, α
 @parameters γ
+@variables a(t), α(t)
 connections = [0 ~ lorenz1.x + lorenz2.y + a*γ,
                α ~ 2lorenz1.x + a*γ]
-connected = ODESystem(connections,t,[a,α],[γ],systems=[lorenz1,lorenz2])               
-connected = structural_simplify(connected)
+sys = ODESystem(connections,t,[a,α],[γ],systems=[lorenz1,lorenz2])               
+sys_simplified = structural_simplify(sys)
 
 u0 = [lorenz1.x => 1.0,
       lorenz1.y => 0.0,
@@ -36,7 +35,7 @@ p  = [lorenz1.σ => 10.0,
       γ => 2.0]
 
 tspan = (0.0,100.0)
-prob = ODEProblem(connected,u0,tspan,p)
+prob = ODEProblem(sys_simplified,u0,tspan,p)
 sol = solve(prob,Rodas4())
 
 @test sol[lorenz1.x] isa Vector
@@ -46,7 +45,14 @@ sol = solve(prob,Rodas4())
 @test sol[α] isa Vector
 @test sol[α,3] isa Float64
 @test length(sol[α,5:10]) == 6
-=#
+
+# Check if indexing using variable names from interpolated solution works
+interpolated_sol = sol(0.0:1.0:10.0)
+@test interpolated_sol[α] isa Vector
+@test interpolated_sol[α,:] isa Vector
+@test interpolated_sol[α,2] isa Float64
+@test length(interpolated_sol[α,1:5]) == 5
+@test interpolated_sol[α] ≈ 2interpolated_sol[lorenz1.x] .+ interpolated_sol[a].*2.0
 
 #=
 using Plots
@@ -56,25 +62,3 @@ plot(sol,vars=(lorenz2.x,α))
 plot(sol,vars=α)
 plot(sol,vars=(t,α))
 =#
-
-@variables t x(t)
-@parameters τ
-D = Differential(t)
-@variables RHS(t)
-@named fol_separate = ODESystem([ RHS  ~ (1 - x)/τ,
-                                  D(x) ~ RHS ])
-fol_simplified = structural_simplify(fol_separate)                                  
-
-prob = ODEProblem(fol_simplified, [x => 0.0], (0.0,10.0), [τ => 3.0])
-sol = solve(prob, Tsit5())
-
-@test sol[x] isa Vector
-@test sol[x,2] isa Float64
-@test sol[x,:] isa Vector
-@test length(sol[x,1:5]) == 5
-@test sol[RHS] isa Vector
-@test sol[RHS,3] isa Float64
-@test length(sol[RHS,5:10]) == 6
-
-interpolated_sol = sol(0.0:1.0:10.0)
-@test interpolated_sol[RHS] ≈ (1 .- interpolated_sol[x])./3.0
