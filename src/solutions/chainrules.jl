c3ccffe4486cac6865f8b1151b0408e1265012a2
Lucas Morton <23484003+lamorton@users.noreply.github.com>
2021-07-18 07:25:04 -0700

    Partial implementation of Zygote AD with symbolic indexing
    Symbolic indexing of state and observed variables. Time index must be
    specified or observed.

---
+++ src/solutions/chainrules.jl
@@ -0,0 +1,72 @@
+function ChainRulesCore.rrule(config::ChainRulesCore.RuleConfig{
+        >:ChainRulesCore.HasReverseMode,
+    },
+    ::typeof(getindex),
+    VA::ODESolution,
+    sym,
+    j::Integer)
+    function ODESolution_getindex_pullback(Δ)
+        i = issymbollike(sym) ? sym_to_index(sym, VA) : sym
+        if i === nothing
+            getter = getobserved(VA)
+            grz = rrule_via_ad(config, getter, sym, VA.u[j], VA.prob.p, VA.t[j])[2](Δ)
+            du = [k == j ? grz[2] : zero(VA.u[1]) for k = 1:length(VA.u)]
+            dp = grz[3] # pullback for p
+            dprob = remake(VA.prob, p = dp)
+            T = eltype(eltype(VA.u))
+            N = length(VA.prob.p)
+            Δ′ = ODESolution{T,N,typeof(du),Nothing,Nothing,Nothing,Nothing,
+                typeof(dprob),Nothing,Nothing,Nothing,Nothing}(du, nothing,
+                nothing, nothing, nothing, dprob, nothing, nothing,
+                VA.dense, 0, nothing, nothing, VA.retcode)
+            (NoTangent(), Δ′, NoTangent(), NoTangent())
+        else
+            du = [m == j ? [i == k ? Δ : zero(VA.u[1][1]) for k = 1:length(VA.u[1])] :
+                  zero(VA.u[1]) for m = 1:length(VA.u)]
+            dp = zero(VA.prob.p)
+            dprob = remake(VA.prob, p = dp)
+            Δ′ = ODESolution{
+                T,
+                N,
+                typeof(du),
+                Nothing,
+                Nothing,
+                typeof(VA.t),
+                typeof(VA.k),
+                typeof(dprob),
+                typeof(VA.alg),
+                typeof(VA.interp),
+                typeof(VA.alg_choice),
+                typeof(VA.destats),
+            }(du,
+                nothing,
+                nothing,
+                VA.t,
+                VA.k,
+                dprob,
+                VA.alg,
+                VA.interp,
+                VA.dense,
+                0,
+                VA.destats,
+                VA.alg_choice,
+                VA.retcode)
+            (NoTangent(), Δ′, NoTangent(), NoTangent())
+        end
+    end
+    VA[sym, j], ODESolution_getindex_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(getindex), VA::ODESolution, sym)
+    function ODESolution_getindex_pullback(Δ)
+        i = issymbollike(sym) ? sym_to_index(sym, VA) : sym
+        if i === nothing
+            throw("AD of purely-symbolic slicing for observed quantities is not yet supported. Work around this by using `A[sym,i]` to access each element sequentially in the function being differentiated.")
+        else
+            Δ′ = [[i == k ? Δ[j] : zero(x[1]) for k = 1:length(x)]
+                  for (x, j) in zip(VA.u, 1:length(VA))]
+            (NoTangent(), Δ′, NoTangent())
+        end
+    end
+    VA[sym], ODESolution_getindex_pullback
+end
--- test/downstream/remake_autodiff.jl
+++ test/downstream/remake_autodiff.jl
@@ -1,20 +1,24 @@
 using OrdinaryDiffEq, ModelingToolkit, Zygote, SciMLSensitivity
 
-@variables t
+@variables t x(t) o(t)
 D = Differential(t)
 function lotka_volterra(; name = name)
-    states = @variables x(t)=1.0 y(t)=1.0
+    states = @variables x(t)=1.0 y(t)=1.0 o(t)
     params = @parameters p1=1.5 p2=1.0 p3=3.0 p4=1.0
     eqs = [
         D(x) ~ p1 * x - p2 * x * y,
         D(y) ~ -p3 * y + p4 * x * y,
+        o ~ x * y,
     ]
     return ODESystem(eqs, t, states, params; name = name)
 end
 
 @named lotka_volterra_sys = lotka_volterra()
+lotka_volterra_sys = structural_simplify(lotka_volterra_sys)
 prob = ODEProblem(lotka_volterra_sys, [], (0.0, 10.0), [])
 sol = solve(prob, Tsit5(), reltol = 1e-6, abstol = 1e-6)
+u0 = [1.0 1.0]
+p = [1.5 1.0 1.0 1.0]
 
 function sum_of_solution(u0, p)
     _prob = remake(prob, u0 = u0, p = p)
@@ -22,6 +26,22 @@ function sum_of_solution(u0, p)
         sensealg = BacksolveAdjoint(autojacvec = ZygoteVJP())))
 end
 
-u0 = [1.0 1.0]
-p = [1.5 1.0 1.0 1.0]
 du01, dp1 = Zygote.gradient(sum_of_solution, u0, p)
+
+function symbolic_indexing(u0, p)
+    _prob = remake(prob, u0 = u0, p = p)
+    soln = solve(_prob, Tsit5(), reltol = 1e-6, abstol = 1e-6, saveat = 0.1,
+        sensealg = BacksolveAdjoint(autojacvec = ZygoteVJP()))
+    sum(soln[x])
+end
+
+du01, dp1 = Zygote.gradient(symbolic_indexing, u0, p)
+
+function symbolic_indexing_observed(u0, p)
+    _prob = remake(prob, u0 = u0, p = p)
+    soln = solve(_prob, Tsit5(), reltol = 1e-6, abstol = 1e-6, saveat = 0.1,
+        sensealg = BacksolveAdjoint(autojacvec = ZygoteVJP()))
+    sum(soln[o, i] for i in 1:length(soln))
+end
+
+du01, dp1 = Zygote.gradient(symbolic_indexing_observed, u0, p)
