d9e497c764d895fd83f84213d2d00070b79718e6
Vedant Puri <vedantpuri@gmail.com>
2021-11-22 18:32:04 -0500

    test

--- test/diffeqoperator.jl
+++ test/diffeqoperator.jl
@@ -1,8 +1,11 @@
 using SciMLBase
+using LinearAlgebra
 
 @testset "DiffEqOperator" begin
     A = rand(10,10);
     @test eachindex(A) === eachindex(SciMLBase.DiffEqArrayOperator(A))
     @test eachindex(A') === eachindex(SciMLBase.DiffEqArrayOperator(A'))
-end
 
+    A = Matrix(I,10,10) |> SciMLBase.DiffEqArrayOperator
+    @test factorize(A) isa SciMLBase.FactorizedDiffEqArrayOperator
+end
