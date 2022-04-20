fb327d1f92b469bf9e747225b7caf2016cc3f0a7
Vedant Puri <vedantpuri@gmail.com>
2022-04-20 10:00:48 -0400

    adjoint(fact) tests

--- test/diffeqoperator.jl
+++ test/diffeqoperator.jl
@@ -4,6 +4,8 @@ using LinearAlgebra
 @testset "DiffEqOperator" begin
     n = 10
     u = rand(n)
+    p = nothing
+    t = 0
 
     A  = rand(n,n)
     At = A'
@@ -11,21 +13,27 @@ using LinearAlgebra
     AA  = SciMLBase.DiffEqArrayOperator(A)
     AAt = AA'
 
+    @test AA  isa SciMLBase.DiffEqArrayOperator
+    @test AAt isa SciMLBase.DiffEqArrayOperator
+
     FF  = factorize(AA)
     FFt = FF'
 
-    p = nothing
-    t = 0
+    @test FF  isa SciMLBase.FactorizedDiffEqArrayOperator
+    @test FFt isa SciMLBase.FactorizedDiffEqArrayOperator
 
     @test eachindex(A)  === eachindex(AA)
     @test eachindex(A') === eachindex(AAt) === eachindex(DiffEqArrayOperator(At))
+
+    @test A  ≈ convert(AbstractMatrix, AA ) ≈ convert(AbstractMatrix, FF )
+    @test At ≈ convert(AbstractMatrix, AAt) ≈ convert(AbstractMatrix, FFt)
+
+    @test A  ≈ Matrix(AA ) ≈ Matrix(FF )
+    @test At ≈ Matrix(AAt) ≈ Matrix(FFt)
 
     @test A  * u ≈ AA(u,p,t)  ≈ FF(u,p,t)
     @test At * u ≈ AAt(u,p,t) ≈ FFt(u,p,t)
 
     @test A  \ u ≈ AA  \ u ≈ FF  \ u
     @test At \ u ≈ AAt \ u ≈ FFt \ u
-
-    A = Matrix(I,n,n) |> SciMLBase.DiffEqArrayOperator
-    @test factorize(A) isa SciMLBase.FactorizedDiffEqArrayOperator
 end
