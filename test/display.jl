0864b8c84aee7113ed20575eaec11eb91952e77a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-09 19:23:31 -0600

    fix bad ODE test function

--- test/display.jl
+++ test/display.jl
@@ -7,7 +7,7 @@ end
 
 
 @testset "Respect :color setting in IOContext" begin
-    local prob = ODEProblem(identity, 1.0, (0.0, 1.0), ones(1,1))
+    local prob = ODEProblem((u,p,t)->u, 1.0, (0.0, 1.0), ones(1,1))
 
     # This test might be too specific
     # @test showtest(prob, true) == "\e[36mODEProblem\e[0m with uType \e[36mFloat64\e[0m and tType \e[36mFloat64\e[0m. In-place: \e[36mfalse\e[0m\ntimespan: (0.0, 1.0)\nu0: 1.0"
