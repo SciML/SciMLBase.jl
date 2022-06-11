03d35b8e9b804974d50e7902d1ab6cb41856675a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-10 22:44:04 -0600

    try again?

--- test/function_building_error_messages.jl
+++ test/function_building_error_messages.jl
@@ -18,7 +18,6 @@ end
 ftoomany(u,p,t,x,y) = 2u
 u0 = 0.5
 tspan = (0.0,1.0)
-ODEProblem(ftoomany,u0,tspan)
 @test_throws SciMLBase.TooManyArgumentsError ODEProblem(ftoomany,u0,tspan)
 
 ftoofew(u,t) = 2u
