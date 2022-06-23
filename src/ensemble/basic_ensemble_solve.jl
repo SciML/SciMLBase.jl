1c2a99c68b22de33328831fd101815b5255bcd1d
Arthur Amorim <48725375+brainsMAKER@users.noreply.github.com>
2022-06-23 15:23:32 +0200

    Fix basic_ensemble_solve.jl to time the first batch
    Move the second `@elapsed` in __solve() in front of a `begin ... end` block.
    Such that EnsembleSolution.elapsedTime includes the simulation time from the first batch.

--- src/ensemble/basic_ensemble_solve.jl
+++ src/ensemble/basic_ensemble_solve.jl
@@ -59,24 +59,25 @@ function __solve(prob::AbstractEnsembleProblem,
   end
 
   converged::Bool = false
-  i = 1
-  II = (batch_size*(i-1)+1):batch_size*i
+  elapsed_time = @elapsed begin
+    i = 1
+    II = (batch_size*(i-1)+1):batch_size*i
 
-  batch_data = solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
-
-  u = prob.u_init === nothing ? similar(batch_data, 0) : prob.u_init
-  u,converged = prob.reduction(u,batch_data,II)
-  elapsed_time = @elapsed for i in 2:num_batches
-    converged && break
-    if i == num_batches
-      II = (batch_size*(i-1)+1):trajectories
-    else
-      II = (batch_size*(i-1)+1):batch_size*i
-    end
     batch_data = solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
-    u,converged = prob.reduction(u,batch_data,II)
-  end
 
+    u = prob.u_init === nothing ? similar(batch_data, 0) : prob.u_init
+    u, converged = prob.reduction(u,batch_data,II)
+    for i in 2:num_batches
+      converged && break
+      if i == num_batches
+        II = (batch_size*(i-1)+1):trajectories
+      else
+        II = (batch_size*(i-1)+1):batch_size*i
+      end
+      batch_data = solve_batch(prob,alg,ensemblealg,II,pmap_batch_size;kwargs...)
+      u, converged = prob.reduction(u,batch_data,II)
+    end
+  end
   _u = tighten_container_eltype(u)
 
   return EnsembleSolution(_u,elapsed_time,converged)
