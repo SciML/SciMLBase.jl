da2d3eb5f2a2ce82c2a71fdd31e9a5c6cb5b7c40
xtalax <alex.lemony@gmail.com>
2022-08-17 18:11:59 +0100

    update interpolation, store grid

--- src/solutions/pde_solutions.jl
+++ src/solutions/pde_solutions.jl
@@ -1,12 +1,13 @@
 """
 $(TYPEDEF)
 """
-struct PDESolution{T, N, uType, Disc, Sol, DType, tType, ivtype, dvType, P, A, IType} <:
+struct PDESolution{T, N, uType, Disc, Sol, DType, tType, gridType, ivtype, dvType, P, A, IType} <:
        AbstractPDESolution{T, N, S}
     u::uType
     original_sol::Sol
     errors::DType
     t::tType
+    ivgrid::gridType
     ivs::ivType
     dvs::dvType
     disc_data::Disc
@@ -24,30 +25,43 @@ function PDESolution(sol::ODESolution{T}, metadata::MOLMetadata) where {T}
     pdesys = metadata.pdesys
     discretespace = metadata.discretespace
 
+    ivs = [discretespace.time, discretespace.x̄...]
+    ivgrid = (isequal(discretespace.time, x) ? sol.t : discretespace.grid[x] for x in ivs)
+    # Reshape the solution to flat arrays
     umap = Dict(map(discretespace.ū) do u
                     let discu = discretespace.discvars[u]
                         solu = map(CartesianIndices(discu)) do I
                             i = sym_to_index(discu[I], odesys)
+                            # Handle Observed
                             if i !== nothing
                                 sol.u[i]
                             else
-                                observed(sol, discu[I])
+                                observed(sol, discu[I], :)
                             end
                         end
                         out = zeros(T, length(sol.t), size(discu)...)
-                        for I in CartesianIndices(discu), i in 1:length(sol.t)
-                            out[i, I] = solu[I][i]
+                        for I in CartesianIndices(discu)
+                            out[:, I] .= solu[I]
                         end
                         u => out
                     end
                 end)
+    # Build Interpolations
+    # TODO: Check if the grid is uniform and use the higher order interpolations that are supported
     interp = Dict(map(keys(umap)) do k
-        k => interpolate(umap[k], BSpline(Cubic()))
+        args = arguments(k) #! Do without Symbolics
+        nodes = ((map(args) do arg
+            i = findfirst(arg, ivs)
+            @assert i !== nothing "Independent variable $arg not found in ivs"
+            ivgrid[i]
+        end)...)
+        k => interpolate(nodes, umap[k], Gridded(Linear()))
     end)
+
     return PDESolution{T, length(discretespace.ū), typeof(umap), typeof(metadata),
-                       typeof(sol), typeof(sol.errors), typeof(sol.t), typeof(pdesys.ivs),
-                       typeof(pdesys.dvs), typeof(sol.prob), typeof(sol.alg),
-                       typeof(interp)}(umap, sol, sol.errors, sol.t, pdesys.ivs,
+                       typeof(sol), typeof(sol.errors), typeof(sol.t),typeof(ivgrid),
+                       typeof(ivs), typeof(pdesys.dvs), typeof(sol.prob), typeof(sol.alg),
+                       typeof(interp)}(umap, sol, sol.errors, sol.t, ivgrid, ivs,
                                            pdesys.dvs, metadata, sol.prob, sol.alg,
                                            interp, sol.dense, sol.tslocation,
                                            sol.retcode)
@@ -56,17 +70,15 @@ end
 wrap_sol(sol::ODESolution, disc_data::MOLMetadata) = PDESolution(sol, disc_data)
 
 function (sol::PDESolution{T, N, S, D})(args...; dv = nothing) where {T, N, S, D <: MOLMetadata}
+    # Colon reconstructs on gridpoints
     args = map(enumerate(args)) do (i, arg)
         if arg isa Colon
-            if i == 1
-                sol.t
-            else
-                sol.disc_data.discretespace.grid[ivs[i-1]]
-            end
+            sol.ivgrid[i]
         else
             arg
         end
     end
+    # If no dv is given, return interpolations for every dv
     if dv === nothing
         @assert length(args) == length(sol.ivs) + 1 "Not enough arguments for the number of independent variables (including time), got $(length(args)) expected $(length(sol.ivs) + 1)."
         return map(dvs) do dv
@@ -74,13 +86,11 @@ function (sol::PDESolution{T, N, S, D})(args...; dv = nothing) where {T, N, S, D
             is = map(ivs) do iv
                 i = findfirst(iv, sol.ivs)
                 @assert i !== nothing "Independent variable $(iv) in dependent variable $(dv) not found in the solution."
-                i + 1
+                i
             end
 
-            sol.interp[dv](first(args), args[is]...)
+            sol.interp[dv](args[is]...)
         end
-
-        return map(dv -> sol.interp[dv](args...), sol.dvs)
     end
     return sol.interp[dv](args...)
 end
@@ -108,10 +118,8 @@ Base.@propagate_inbounds function Base.getindex(A::PDESolution{T, N, S, D},
         if idv !== nothing
             dv = A.dvs[idv]
         end
-        if issymbollike(sym) && indepsym !== nothing && Symbol(sym) == indepsym
-            A.t
-        elseif issymbollike(sym) && iv !== nothing && isequal(sym, iv)
-            A.disc_data.discretespace.grid[sym]
+        if issymbollike(sym) && iv !== nothing && isequal(sym, iv)
+            A.ivgrid[iiv]
         elseif issymbollike(sym) && dv !== nothing && isequal(sym, dv)
             A.u[sym]
         else
