57736740374b632079bd9015a7907ef45134dac2
xtalax <alex.lemony@gmail.com>
2022-08-17 19:45:05 +0100

    format

--- src/discretizers/methodoflines_metadata.jl
+++ src/discretizers/methodoflines_metadata.jl
@@ -1,10 +1,12 @@
 """
 $(TYPEDEF)
 
-A type used to store data about a PDESystem, and how it was discretized by MethodOfLines.jl. Used to unpack the solution.
+A type used to store data about a PDESystem, and how it was discretized by MethodOfLines.jl.
+Used to unpack the solution.
 
 - `discretespace`: a DiscreteSpace object, used in the discretization.
-- `disc`: a Discretization object, used in the discretization. Usually a MOLFiniteDifference object.
+- `disc`: a Discretization object, used in the discretization. Usually a
+          MOLFiniteDifference object.
 - `pdesys`: a PDESystem object, used in the discretization.
 """
 struct MOLMetadata{N, M, Ds, Disc, PDE} <: AbstractDiscretizerMetadata{N, M}
