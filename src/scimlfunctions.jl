5379ac471583912878938ac6ac1aabddb082494b
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-28 15:14:46 -0400

    more robust

--- src/scimlfunctions.jl
+++ src/scimlfunctions.jl
@@ -1749,7 +1749,11 @@ function ODEFunction{iip, recompile}(f;
         mass_matrix = ((I for i in 1:length(f))...,)
     end
 
-    if (recompile === FunctionWrapperSpecialize) || (recompile == false) &&
+    if recompile === false
+      recompile = FunctionWrapperSpecialize
+    end
+
+    if (recompile === FunctionWrapperSpecialize) &&
        !(f isa FunctionWrappersWrappers.FunctionWrappersWrapper)
         if iip
             f = wrapfun_iip(f)
