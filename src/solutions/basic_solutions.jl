32a7dc7c618ad845a0080ee4cd3385f53cabfd75
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-09-14 23:41:26 -0400

    More robust hotfix

--- src/solutions/basic_solutions.jl
+++ src/solutions/basic_solutions.jl
@@ -79,7 +79,7 @@ function build_solution(prob::AbstractIntegralProblem,
 end
 
 function wrap_sol(sol)
-    if hasfield(typeof(sol.prob), :problem_type)
+    if hasproperty(typeof(sol), :prob) && hasproperty(typeof(sol.prob), :problem_type)
         wrap_sol(sol, sol.prob.problem_type)
     else
         sol
