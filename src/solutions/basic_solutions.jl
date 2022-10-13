6680c85ca7a946955bfe8ce31b00d3da273b8d51
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-10-13 11:53:27 +0200

    Update src/solutions/basic_solutions.jl
    Co-authored-by: Fredrik Ekre <ekrefredrik@gmail.com>

--- src/solutions/basic_solutions.jl
+++ src/solutions/basic_solutions.jl
@@ -29,7 +29,7 @@ struct LinearSolution{T, N, uType, R, A, C} <: AbstractLinearSolution{T, N}
 end
 
 function build_linear_solution(alg, u, resid, cache;
-                               retcode = Default,
+                               retcode = ReturnCode.Default,
                                iters = 0)
     T = eltype(eltype(u))
     N = length((size(u)...,))
