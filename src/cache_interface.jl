9b3f06f42c4915295f313f01101810729e7e0bda
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-10-23 15:26:59 +0200

    provide default implementation

--- src/cache_interface.jl
+++ src/cache_interface.jl
@@ -1,15 +1,7 @@
-function get_p(::AbstractOptimizationCache)
-    error("`get_p`: method has not been implemented for the cache")
-end
-function get_observed(::AbstractOptimizationCache)
-    error("`get_observed`: method has not been implemented for the cache")
-end
-function get_syms(::AbstractOptimizationCache)
-    error("`get_syms`: method has not been implemented for the cache")
-end
-function get_paramsyms(::AbstractOptimizationCache)
-    error("`get_paramsyms`: method has not been implemented for the cache")
-end
+get_p(cache::AbstractOptimizationCache) = cache.p
+get_observed(cache::AbstractOptimizationCache) = cache.f.observed
+get_syms(cache::AbstractOptimizationCache) = cache.f.syms
+get_paramsyms(cache::AbstractOptimizationCache)= cache.f.paramsyms
 
 has_observed(cache::AbstractOptimizationCache) = get_observed(cache) !== nothing
 has_syms(cache::AbstractOptimizationCache) = get_syms(cache) !== nothing
