aa021dc70df53a72145296a96961ba343fa55da1
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-24 00:58:29 +0200

    fix another typo

--- src/problems/basic_problems.jl
+++ src/problems/basic_problems.jl
@@ -196,7 +196,7 @@ $(SIGNATURES)
 
 Define a nonlinear problem from a standard ODE problem. Note that
 this is interpreted in the form of the steady state problem, i.e.
-find the ODE's solution at time `t = \infty`
+find the ODE's solution at time ``t = \\infty``
 """
 function NolinearProblem(prob::AbstractODEProblem)
     NolinearProblem{isinplace(prob)}(prob.f, prob.u0, prob.p)
