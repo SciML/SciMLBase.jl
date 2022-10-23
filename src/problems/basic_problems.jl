f7a9525e327642ffd45369232ec92edb4d5006cd
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-23 19:17:07 +0200

    t infty

--- src/problems/basic_problems.jl
+++ src/problems/basic_problems.jl
@@ -185,7 +185,7 @@ $(SIGNATURES)
 Define a nonlinear problem using an instance of
 [`AbstractODEFunction`](@ref AbstractODEFunction). Note that
 this is interpreted in the form of the steady state problem, i.e.
-find the ODE's solution at time ``t = \infty``.
+find the ODE's solution at time ``t = \\infty``.
 """
 function NolinearProblem(f::AbstractODEFunction, u0, p = NullParameters(); kwargs...)
     NolinearProblem{isinplace(f)}(f, u0, p; kwargs...)
