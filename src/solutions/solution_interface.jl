a9f9f55971a32798bdd160b0aae41707ef14c755
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-11-05 02:13:19 +0100

    Revert "Revert "Fix integrator parameter indexing, enable tests""

--- src/solutions/solution_interface.jl
+++ src/solutions/solution_interface.jl
@@ -61,11 +61,10 @@ Base.@propagate_inbounds function Base.getindex(A::AbstractTimeseriesSolution,
 end
 
 Base.@propagate_inbounds function Base.getindex(A::AbstractTimeseriesSolution, sym)
-    if issymbollike(sym) || all(issymbollike, sym)
-        if sym isa AbstractArray
-            return map(s -> A[s], collect(sym))
-        end
+    if issymbollike(sym)
         i = sym_to_index(sym, A)
+    elseif all(issymbollike, sym)
+        return getindex.((A,), sym)
     else
         i = sym
     end
