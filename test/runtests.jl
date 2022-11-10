9374ae376e0e4fe970ccdb8855a4453d5f7b113d
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-11-10 16:11:43 -0500

    add missing test (#308)

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,5 +37,6 @@ end
         @time @safetestset "Symbol and integer based indexing of integrators" begin include("downstream/integrator_indexing.jl") end
         @time @safetestset "Unitful interoplations" begin include("downstream/unitful_interpolations.jl") end
         @time @safetestset "Integer idxs" begin include("downstream/integer_idxs.jl") end
+        @time @safetestset "Autodiff Remake" begin include("downstream/remake_autodiff.jl") end
     end
 end
