0d36befb6ecc57b3e501e4c220637262aa535680
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-11-13 18:21:27 +0100

    removes wrong doc

--- src/solutions/optimization_solutions.jl
+++ src/solutions/optimization_solutions.jl
@@ -12,10 +12,7 @@ Representation of the solution to an nonlinear optimization defined by an Optimi
 - `alg`: the algorithm type used by the solver.
 - `original`: if the solver is wrapped from an alternative solver ecosystem, such as
   Optim.jl, then this is the original return from said solver library.
-- `retcode`: the return code from the solver. Used to determine whether the solver solved
-  successfully (`sol.retcode === ReturnCode.Success`), whether it terminated due to a user-defined
-  callback (`sol.retcode === :Terminated`), or whether it exited due to an error. For more
-  details, see the return code section of the DifferentialEquations.jl documentation.
+- `retcode`: the return code from the solver.
 """
 struct OptimizationSolution{T, N, uType, C <: AbstractOptimizationCache, A, Tf, O} <:
        AbstractOptimizationSolution{T, N}
