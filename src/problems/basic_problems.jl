6de98907e973e74fb879daf8b15cad6ba863100f
Arno Strouwen <arno.strouwen@telenet.be>
2023-01-03 23:33:48 +0100

    LinearProblem docstring

--- src/problems/basic_problems.jl
+++ src/problems/basic_problems.jl
@@ -18,7 +18,7 @@ Au = b
 
 For matrix-free versions, the specification of the problem is given by an
 operator `A(u,p,t)` which computes `A*u`, or in-place as `A(du,u,p,t)`. These
-are specified via the `AbstractSciMLOperator` interface. For more details, see
+are specified via the `AbstractDiffEqOperator` interface. For more details, see
 the [SciMLBase Documentation](https://docs.sciml.ai/SciMLBase/stable/).
 
 Note that matrix-free versions of LinearProblem definitions are not compatible
@@ -32,13 +32,13 @@ Optionally, an initial guess ``u₀`` can be supplied which is used for iterativ
 methods.
 
 ```julia
-LinearProblem(A,b;u0=nothing,kwargs...)
-LinearProblem{isinplace}(f::AbstractSciMLOperator,b,p=NullParameters();u0=nothing,kwargs...)
+LinearProblem(A,b,p=NullParameters();u0=nothing,kwargs...)
+LinearProblem{isinplace}(f::AbstractDiffEqOperator,b,p=NullParameters();u0=nothing,kwargs...)
 ```
 
 `isinplace` optionally sets whether the function is in-place or not, i.e. whether
 the solvers are allowed to mutate. By default this is true for `AbstractMatrix`,
-and for `AbstractSciMLOperator`s it matches the choice of the operator definition.
+and for `AbstractDiffEqOperator`s it matches the choice of the operator definition.
 
 Parameters are optional, and if not given, then a `NullParameters()` singleton
 will be used, which will throw nice errors if you try to index non-existent
@@ -48,7 +48,7 @@ parameters. Any extra keyword arguments are passed on to the solvers.
 
 * `A`: The representation of the linear operator.
 * `b`: The right-hand side of the linear system.
-* `p`: The parameters for the problem. Defaults to `NullParameters`. Currently unused.
+* `p`: The parameters for the problem. Defaults to `NullParameters`.
 * `u0`: The initial condition used by iterative solvers.
 * `kwargs`: The keyword arguments passed on to the solvers.
 """
