31d0df951d417ecc69017e2d6a84ccffa9786ad4
Arno Strouwen <arno.strouwen@telenet.be>
2023-01-05 08:19:39 +0100

    [skip ci] spelling

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -177,14 +177,14 @@ VectorContinuousCallback(condition,affect!,len;
                    abstol=10eps(),reltol=0,repeat_nudge=1//100)
 ```
 
-This is also a subtype of `AbstractContinuousCallback`. `CallbackSet` is not feasible when you have a large number of callbacks,
+This is also a subtype of `AbstractContinuousCallback`. `CallbackSet` is not feasible when you have many callbacks,
 as it doesn't scale well. For this reason, we have `VectorContinuousCallback` - it allows you to have a single callback for
 multiple events.
 
 # Arguments
 
 - `condition`: This is a function `condition(out, u, t, integrator)` which should save the condition value in the array `out`
-   at the right index. Maximum index of `out` should be specified in the `len` property of callback. So this way you can have
+   at the right index. Maximum index of `out` should be specified in the `len` property of callback. So, this way you can have
    a chain of `len` events, which would cause the `i`th event to trigger when `out[i] = 0`.
 - `affect!`: This is a function `affect!(integrator, event_index)` which lets you modify `integrator` and it tells you about
    which event occurred using `event_idx` i.e. gives you index `i` for which `out[i]` came out to be zero.
