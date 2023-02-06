563c3c8c36d1d10ce791ec62aaadcc0bd783af62
xtalax <alex.lemony@gmail.com>
2023-02-06 18:01:21 +0000

    downstream test

--- test/downstream/symbol_indexing.jl
+++ test/downstream/symbol_indexing.jl
@@ -222,3 +222,41 @@ sol = solve(prob, Tsit5())
     @test sol[a] ≈ 1
     @test sol[b] ≈ 100
 end
+
+@testset "Problem indexing" begin
+    @parameters σ ρ β
+    @variables t x(t) y(t) z(t)
+    D = Differential(t)
+
+    eqs = [D(D(x)) ~ σ * (y - x),
+    D(y) ~ x * (ρ - z) - y,
+    D(z) ~ x * y - β * z]
+
+    @named sys = ODESystem(eqs)
+
+    u0 = [D(x) => 2.0,
+    x => 1.0,
+    y => 0.0,
+    z => 0.0]
+
+    p = [σ => 28.0,
+    ρ => 10.0,
+    β => 8 / 3]
+
+    tspan = (0.0, 100.0)
+    prob = ODEProblem(sys, u0, tspan, p, jac = true)
+
+    @test prob[σ] == 28.0
+    @test prob[ρ] == 10.0
+    @test prob[β] == 8 / 3
+
+    @test prob[x] == 1.0
+    @test prob[y] == 0.0
+    @test prob[z] == 0.0
+
+    prob[y] = 1.0
+    @test prob[y] == 1.0
+
+    prob[σ] = 1.0
+    @test prob[σ] == 1.0
+end
