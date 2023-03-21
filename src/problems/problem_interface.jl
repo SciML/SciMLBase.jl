c8596b8fe6dc15f99a1f9e4aeb1bd9cd4be5b968
Torkel <torkel.loman@gmail.com>
2023-03-21 11:32:33 -0400

    enable missing setindex!(prob,val,sym)

--- src/problems/problem_interface.jl
+++ src/problems/problem_interface.jl
@@ -34,18 +34,42 @@ function Base.setindex!(prob::AbstractSciMLProblem, val, sym)
             if !isnothing(i)
                 prob.u0[i] = val
                 return prob
+            elseif sym isa Symbol  # Hanldes input like :X.
+                s_f = Symbol.(getfield.(states(prob.f.sys),:f))
+                if count(isequal(Symbol(sym)), s_f) == 1
+                    i = findfirst(isequal(sym), s_f)
+                    prob.u0[i] = val
+                    return prob
+                elseif count(isequal(Symbol(sym)), s_f) > 1
+                    error("The input symbol $(sym) occurs several times among problem states. Please avoid use Symbol form (:$(sym)).")
+                end              
+            elseif count('₊', String(Symbol(sym))) == 1  # Handles input like sys.X. 
+                s_names = Symbol.(prob.f.sys.name,:₊,s)
+                if count(isequal(Symbol(sym)), s_names) == 1
+                    i = findfirst(isequal(Symbol(sym)), s_names)
+                    prob.u0[i] = val
+                    return prob
+                end
             end
 
             i = findfirst(isequal(Symbol(sym)), params)
             if !isnothing(i)
                 prob.p[i] = val
                 return prob
+            elseif count('₊', String(Symbol(sym))) == 1  # Handles input like sys.X. 
+                p_names = Symbol.(prob.f.sys.name,:₊,params)
+                if count(isequal(Symbol(sym)), p_names) == 1
+                    i = findfirst(isequal(Symbol(sym)), p_names)
+                    prob.p[i] = val
+                    return prob
+                end
             end
             error("Invalid indexing of problem: $sym is not a state or parameter, it may be an observed variable.")
         else
             error("Invalid indexing of problem: $sym is not a symbol")
         end
     else
+        println("HERE 2")
         error("Invalid indexing of problem: Problem does not support indexing without a system")
     end
 end
