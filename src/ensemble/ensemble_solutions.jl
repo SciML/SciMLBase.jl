f66e4bbb640ef1ef89850ab3e74e5fbf668e7533
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-03-30 06:07:39 -0400

    Update ensemble_solutions.jl

--- src/ensemble/ensemble_solutions.jl
+++ src/ensemble/ensemble_solutions.jl
@@ -44,7 +44,7 @@ function EnsembleSolution(sim::T, elapsedTime,
                      typeof(sim)}(sim, elapsedTime, converged)
 end
 
-Base.reverse(sim::EnsembleSolution) = EnsembleSolution(reverse(sim.u), sim.elapsedTime, sim.converged)\
+Base.reverse(sim::EnsembleSolution) = EnsembleSolution(reverse(sim.u), sim.elapsedTime, sim.converged)
 
 """
 $(TYPEDEF)
