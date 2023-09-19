819ec920ac894040c0687de94bbcc97b419a98d4
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-09-19 03:08:44 -0400

    Update src/problems/basic_problems.jl

--- src/problems/basic_problems.jl
+++ src/problems/basic_problems.jl
@@ -437,7 +437,7 @@ struct SampledIntegralProblem{Y, X, D, K} <: AbstractIntegralProblem{false}
         @assert dim <= ndims(y) "The integration dimension `dim` is larger than the number of dimensions of the integrand `y`"
         @assert length(x)==size(y, dim) "The integrand `y` must have the same length as the sampling points `x` along the integrated dimension."
         @assert axes(x, 1)==axes(y, dim) "The integrand `y` must obey the same indexing as the sampling points `x` along the integrated dimension."
-        new{typeof(y), typeof(x), typeof(dim)}, typeof(kwargs)}(y, x, dim, kwargs)
+        new{typeof(y), typeof(x), typeof(dim), typeof(kwargs)}(y, x, dim, kwargs)
     end
 end
 
