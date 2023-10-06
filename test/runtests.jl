a9ba6aee9b99fa9ee895675de5fd69d4f513f154
Lilith Hafner <Lilith.Hafner@gmail.com>
2023-10-06 09:18:26 -0500

    configure PythonCall to use the same interpreter as PyCall to avoid the spooky action at a distance

--- test/runtests.jl
+++ test/runtests.jl
@@ -103,7 +103,7 @@ end
     if !is_APPVEYOR && GROUP == "Python"
         activate_python_env()
         @time @safetestset "PyCall" begin
-            # include("python/pycall.jl")
+            include("python/pycall.jl")
         end
         @time @safetestset "PythonCall" begin
             include("python/pythoncall.jl")
