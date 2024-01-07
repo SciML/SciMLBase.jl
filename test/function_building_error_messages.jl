ac2ce86b7b108476fac341e55390b666c71b8adc
Arno Strouwen <arno.strouwen@telenet.be>
2024-01-07 13:03:57 +0100

    fix deprecation warnings in core

--- test/function_building_error_messages.jl
+++ test/function_building_error_messages.jl
@@ -462,14 +462,14 @@ NonlinearFunction(nfoop, vjp = nvjp)
 
 # Integrals
 intf(u) = 1.0
-@test_throws SciMLBase.TooFewArgumentsError IntegralProblem(intf, 0.0, 1.0)
+@test_throws SciMLBase.TooFewArgumentsError IntegralProblem(intf, (0.0, 1.0))
 intf(u, p) = 1.0
 p = 2.0
 
-IntegralProblem(intf, 0.0, 1.0)
-IntegralProblem(intf, 0.0, 1.0, p)
-IntegralProblem(intf, [0.0], [1.0])
-IntegralProblem(intf, [0.0], [1.0], p)
+IntegralProblem(intf, (0.0, 1.0))
+IntegralProblem(intf, (0.0, 1.0), p)
+IntegralProblem(intf, ([0.0], [1.0]))
+IntegralProblem(intf, ([0.0], [1.0]), p)
 
 x = [1.0, 2.0]
 y = rand(2, 2)
