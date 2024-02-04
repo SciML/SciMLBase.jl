7d9bf90e3375668732b13844f0201c6e9863ca58
ArnoStrouwen <arno.strouwen@telenet.be>
2024-02-04 01:03:31 +0100

    make sensitivity_solution work with Enzyme

--- src/solutions/ode_solutions.jl
+++ src/solutions/ode_solutions.jl
@@ -399,9 +399,8 @@ function sensitivity_solution(sol::ODESolution, u, t)
     end
 
     interp = enable_interpolation_sensitivitymode(sol.interp)
-
-    ODESolution{T, N}(u, sol.u_analytic, sol.errors, t,
-        nothing, sol.prob,
+    ODESolution{T, N}(u, sol.u_analytic, sol.errors, collect(t),
+        sol.k, sol.prob,
         sol.alg, interp,
         sol.dense, sol.tslocation,
         sol.stats, sol.alg_choice, sol.retcode)
