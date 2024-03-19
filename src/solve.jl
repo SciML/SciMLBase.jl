79817b3f7a8fb9d11396d6a6c7180192e88b225a
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-03-20 01:35:53 +0530

    Update solve.jl

--- src/solve.jl
+++ src/solve.jl
@@ -116,13 +116,13 @@ function _check_opt_alg(prob::OptimizationProblem, alg; kwargs...)
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) requires constraints, pass them with the `cons` kwarg in `OptimizationFunction`."))
     !allowscallback(alg) && haskey(kwargs, :callback) &&
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) does not support callbacks, remove the `callback` keyword argument from the `solve` call."))
-    !requiresgradient(alg) && !(prob.f::Type{OptimizationFunction}) &&
+    !requiresgradient(alg) && !(prob.f::OptimizationFunction) &&
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) requires gradients, pass them in `OptimizationFunction`."))
-    !requireshessian(alg) && !(prob.f::Type{OptimizationFunction}) &&
+    !requireshessian(alg) && !(prob.f::OptimizationFunction) &&
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) requires hessians, pass them in `OptimizationFunction`."))
-    !requiresconsjac(alg) && !(prob.f::Type{OptimizationFunction}) &&
+    !requiresconsjac(alg) && !(prob.f::OptimizationFunction) &&
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) requires constraint jacobian, pass them with the `cons` kwarg in `OptimizationFunction`."))
-    !requiresconshess(alg) && !(prob.f::Type{OptimizationFunction}) &&
+    !requiresconshess(alg) && !(prob.f::OptimizationFunction) &&
         throw(IncompatibleOptimizerError("The algorithm $(typeof(alg)) requires constraint hessian, pass them with the `cons` kwarg in `OptimizationFunction`."))
     return
 end
