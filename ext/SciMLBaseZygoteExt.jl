b29ccf2965caa5d438687d5e8b8facc11182d5d0
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-12 10:57:45 -0400

    Update SciMLBaseZygoteExt.jl

--- ext/SciMLBaseZygoteExt.jl
+++ ext/SciMLBaseZygoteExt.jl
@@ -217,7 +217,7 @@ end
     VA[sym], ODESolution_getindex_pullback
 end
 
-@adjoint function ODESolution{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12
+@adjoint function ODESolution{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14
 }(u,
         args...) where {T1, T2, T3, T4, T5, T6, T7, T8,
         T9, T10, T11, T12, T13, T14}
