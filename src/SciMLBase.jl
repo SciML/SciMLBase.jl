5de5d1be42045a3598015292cbf9e8561ee66c6d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-07-08 02:23:17 +0530

    feat: move clock interface here

--- src/SciMLBase.jl
+++ src/SciMLBase.jl
@@ -23,6 +23,7 @@ import RuntimeGeneratedFunctions
 import EnumX
 import ADTypes: AbstractADType
 import Accessors: @set, @reset
+using Expronicon.ADT: @match
 
 using Reexport
 using SciMLOperators
@@ -717,6 +718,7 @@ include("problems/problem_traits.jl")
 include("problems/problem_interface.jl")
 include("problems/optimization_problems.jl")
 
+include("clock.jl")
 include("solutions/basic_solutions.jl")
 include("solutions/nonlinear_solutions.jl")
 include("solutions/ode_solutions.jl")
@@ -834,5 +836,7 @@ export step!, deleteat!, addat!, get_tmp_cache,
        isdiscrete, reeval_internals_due_to_modification!
 
 export ContinuousCallback, DiscreteCallback, CallbackSet, VectorContinuousCallback
+
+export Clocks, TimeDomain, is_discrete_time_domain, isclock, issolverstepclock, iscontinuous
 
 end
