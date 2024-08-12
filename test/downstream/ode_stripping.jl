baed4879bf4dbbb5896b6899dae79727a9554e60
jClugstor <jadonclugston@gmail.com>
2024-08-12 12:00:04 -0400

    add test for ode_stripping

---
+++ test/downstream/ode_stripping.jl
@@ -0,0 +1,8 @@
+using OrdinaryDiffEq
+f(u, p, t) = 1.01 * u
+u0 = 1 / 2
+tspan = (0.0, 1.0)
+prob = ODEProblem(f, u0, tspan)
+sol = solve(prob, Tsit5(), reltol = 1e-8, abstol = 1e-8)
+
+@test isnothing(strip_solution(sol).f)
