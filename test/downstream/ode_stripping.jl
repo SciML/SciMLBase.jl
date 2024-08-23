692e7168391fc2dfbc6771992398484ff5a5fb1a
jClugstor <jadonclugston@gmail.com>
2024-08-23 13:02:29 -0400

    strip_solution test

--- test/downstream/ode_stripping.jl
+++ test/downstream/ode_stripping.jl
@@ -1,10 +1,19 @@
 using OrdinaryDiffEq
-f(u, p, t) = 1.01 * u
-u0 = 1 / 2
-tspan = (0.0, 1.0)
-prob = ODEProblem(f, u0, tspan)
-sol = solve(prob, Tsit5(), reltol = 1e-8, abstol = 1e-8)
+
+function lorenz!(du, u, p, t)
+    du[1] = 10.0 * (u[2] - u[1])
+    du[2] = u[1] * (28.0 - u[3]) - u[2]
+    du[3] = u[1] * u[2] - (8 / 3) * u[3]
+end
+
+u0 = [1.0; 0.0; 0.0]
+tspan = (0.0, 100.0)
+prob = ODEProblem(lorenz!, u0, tspan)
+# implicit solver so we can test cache stripping worked
+sol = solve(prob, Rosenbrock23())
 
 @test isnothing(strip_solution(sol).f)
 
 @test isnothing(strip_solution(sol).interp.f)
+
+@test isnothing(strip_solution(sol).interp.cache.jac_config)
