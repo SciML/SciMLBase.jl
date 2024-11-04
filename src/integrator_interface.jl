8efe9a451ff7fd33e8caf76ce07de0ed1dff299e
Benjamin Chung <ckfinite@gmail.com>
2024-11-04 15:04:48 -0800

    Rename reinitialization_method to reinitialize_alg

--- src/integrator_interface.jl
+++ src/integrator_interface.jl
@@ -308,7 +308,7 @@ function auto_dt_reset!(integrator::DEIntegrator)
 end
 
 """
-    change_t_via_interpolation!(integrator::DEIntegrator,t,modify_save_endpoint=Val{false},reinitialization_method=nothing)
+    change_t_via_interpolation!(integrator::DEIntegrator,t,modify_save_endpoint=Val{false},reinitialize_alg=nothing)
 
 Modifies the current `t` and changes all of the corresponding values using the local interpolation. If the current solution
 has already been saved, one can provide the optional value `modify_save_endpoint` to also modify the endpoint of `sol` in the
