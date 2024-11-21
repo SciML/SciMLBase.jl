b072a122b7c07265503348c91200e4e90924d371
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-11-21 15:40:55 +0530

    fix: fix `initializeprobpmap` call in `OverrideInit`

--- test/downstream/initialization.jl
+++ test/downstream/initialization.jl
@@ -1,4 +1,5 @@
-using OrdinaryDiffEq, Sundials, SciMLBase, Test
+using ModelingToolkit, NonlinearSolve, OrdinaryDiffEq, Sundials, SciMLBase, Test
+using ModelingToolkit: t_nounits as t, D_nounits as D
 
 @testset "CheckInit" begin
     abstol = 1e-10
@@ -58,4 +59,27 @@ using OrdinaryDiffEq, Sundials, SciMLBase, Test
                 prob, integ, f, SciMLBase.CheckInit(), Val(SciMLBase.isinplace(f)); abstol)
         end
     end
+end
+
+@testset "OverrideInit with MTK" begin
+    abstol = 1e-10
+    reltol = 1e-8
+
+    @variables x(t) [guess = 1.0] y(t) [guess = 1.0]
+    @parameters p=missing [guess = 1.0] q=missing [guess = 1.0]
+    @mtkbuild sys = ODESystem([D(x) ~ p * y + q * t, D(y) ~ 5x + q], t;
+        initialization_eqs = [p^2 + q^2 ~ 3, x^3 + y^3 ~ 5])
+    prob = ODEProblem(
+        sys, [x => 1.0], (0.0, 1.0), [p => 1.0]; initializealg = SciMLBase.NoInit())
+
+    @test prob.f.initialization_data isa SciMLBase.OverrideInitData
+    integ = init(prob, Tsit5())
+    u0, pobj, success = SciMLBase.get_initial_values(
+        prob, integ, prob.f, SciMLBase.OverrideInit(), Val(true);
+        nlsolve_alg = NewtonRaphson(), abstol, reltol)
+
+    @test getu(sys, x)(u0) ≈ 1.0
+    @test getu(sys, y)(u0) ≈ cbrt(4)
+    @test getp(sys, p)(pobj) ≈ 1.0
+    @test getp(sys, q)(pobj) ≈ sqrt(2)
 end
