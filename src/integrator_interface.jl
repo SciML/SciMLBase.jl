dd890607a0ee32f7abea6a06d95995a2cc245a9f
termi-official <9196588+termi-official@users.noreply.github.com>
2024-12-22 21:39:34 +0100

    Fix typo in set_proposed_dt! docstring

--- src/integrator_interface.jl
+++ src/integrator_interface.jl
@@ -157,8 +157,8 @@ function get_proposed_dt(i::DEIntegrator)
 end
 
 """
-    set_proposed_dt(i::DEIntegrator,dt)
-    set_proposed_dt(i::DEIntegrator,i2::DEIntegrator)
+    set_proposed_dt!(i::DEIntegrator,dt)
+    set_proposed_dt!(i::DEIntegrator,i2::DEIntegrator)
 
 Sets the proposed `dt` for the next timestep. If the second argument isa `DEIntegrator`, then it sets the timestepping of
 the first argument to match that of the second one. Note that due to PI control and step acceleration, this is more than matching
