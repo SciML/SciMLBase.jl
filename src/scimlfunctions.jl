88c4b560d393ff62ab26f581d73cceca68934acc
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-01-03 11:30:49 +0530

    feat: forward `getproperty` on `HomotopyNonlinearFunction` to wrapped `NonlinearFunction`

--- src/scimlfunctions.jl
+++ src/scimlfunctions.jl
@@ -4855,6 +4855,22 @@ function SymbolicIndexingInterface.observed(fn::HomotopyNonlinearFunction, sym::
     SymbolicIndexingInterface.observed(symbolic_container(fn), sym)
 end
 
+function Base.getproperty(x::HomotopyNonlinearFunction, sym::Symbol)
+  if hasfield(HomotopyNonlinearFunction, sym)
+    return getfield(x, sym)
+  elseif hasfield(NonlinearFunction, sym)
+    return getfield(getfield(x, :f), sym)
+  elseif (sym == :initializeprob || sym == :update_initializeprob! ||
+        sym == :initializeprobmap || sym == :initializeprobpmap)
+    f = getfield(x, :f)
+    if f.initialization_data === nothing
+      return nothing
+    else
+      return getproperty(f.initialization_data, sym)
+    end
+  end
+end
+
 function Base.getproperty(x::AbstractSciMLFunction, sym::Symbol)
     if __has_initialization_data(x) &&
        (sym == :initializeprob || sym == :update_initializeprob! ||
