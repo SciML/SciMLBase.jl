1428c2d0e4fe2b22a19b8b39e3254d33487d0182
Vincent Du <54586336+vyudu@users.noreply.github.com>
2025-03-19 14:50:18 -0400

    Update test/downstream/solution_interface.jl
    Co-authored-by: Aayush Sabharwal <aayush.sabharwal@gmail.com>

--- test/downstream/solution_interface.jl
+++ test/downstream/solution_interface.jl
@@ -215,7 +215,7 @@ end
             @test sol[x] == xvals
             @test is_parameter(sol, p)
             @test parameter_index(sol, p) == parameter_index(sys, p)
-            @test isequal(parameter_symbols(sol)[1], p)
+            @test any(isequal(p), parameter_symbols(sol))
             @test is_independent_variable(sol, t)
 
             tmp = copy(prob.u0)
