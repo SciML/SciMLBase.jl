c5f82f98f7a358b10cd1cea780a48d7950777aac
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-04-08 16:20:38 +0530

    chore: pass p directly

--- ext/SciMLBaseZygoteExt.jl
+++ ext/SciMLBaseZygoteExt.jl
@@ -85,15 +85,13 @@ end
         if is_observed(VA, sym)
             f = observed(VA, sym)
             p = parameter_values(VA)
-            tunables, repack, _ = SciMLStructures.canonicalize(SciMLStructures.Tunable(), p)
             u = state_values(VA)
             t = current_time(VA)
-            y, back = Zygote.pullback(u, tunables) do u, tunables
-                _p = repack(tunables)
-                f.(u, Ref(_p), t)
+            y, back = Zygote.pullback(u, p) do u, p
+                f.(u, Ref(p), t)
             end
             gs = back(Δ)
-            ((u = gs[1], prob = (p = (tunable = gs[2],),)), nothing)
+            (u = gs[1], prob = (p = gs[2],),), nothing
         elseif i === nothing
             throw(error("Zygote AD of purely-symbolic slicing for observed quantities is not yet supported. Work around this by using `A[sym,i]` to access each element sequentially in the function being differentiated."))
         else
