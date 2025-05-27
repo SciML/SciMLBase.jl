b9120859c3b123c2c13877bdc1906805fbde6199
jClugstor <jadonclugston@gmail.com>
2025-05-27 14:29:47 -0400

    add missing type parameter for unwrapped_f of NoSpecialize

--- src/scimlfunctions.jl
+++ src/scimlfunctions.jl
@@ -2780,10 +2780,11 @@ ODEFunction(f; kwargs...) = ODEFunction{isinplace(f, 4), FullSpecialize}(f; kwar
 ODEFunction(f::ODEFunction; kwargs...) = f
 
 function unwrapped_f(f::ODEFunction, newf = unwrapped_f(f.f))
+    Main.@infiltrate
     if specialization(f) === NoSpecialize
         ODEFunction{isinplace(f), specialization(f), Any, Any, Any,
             Any, Any, Any, Any, typeof(f.jac_prototype),
-            typeof(f.sparsity), Any, Any, Any,
+            typeof(f.sparsity), Any, Any, Any, Any,
             Any, typeof(f.colorvec),
             typeof(f.sys), Union{Nothing, OverrideInitData}, Union{Nothing, ODE_NLProbData}}(
             newf, f.mass_matrix, f.analytic, f.tgrad, f.jac,
