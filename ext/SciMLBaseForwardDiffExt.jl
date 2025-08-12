4714ed4d623955814f1effb61a1a96c69dd11a30
jClugstor <jadonclugston@gmail.com>
2025-08-12 16:30:04 -0400

    add utilities to SciMLBase

--- ext/SciMLBaseForwardDiffExt.jl
+++ ext/SciMLBaseForwardDiffExt.jl
@@ -3,13 +3,16 @@ module SciMLBaseForwardDiffExt
 using SciMLBase, ForwardDiff
 using ArrayInterface
 
-import SciMLBase: hasdualpromote, wrapfun_oop, wrapfun_iip, prob2dtmin, isdualtype, value
+import SciMLBase: 
+    wrapfun_oop, wrapfun_iip, isdualtype, value, DualEltypeChecker,
+    AbstractTimeseriesSolution, NonlinearProblem, NonlinearLeastSquaresProblem,
+    ODEProblem, SDEProblem, RODEProblem, DDEProblem, PDEProblem, DAEProblem,
+    RecursiveArrayTools, totallength
+
 
 
 eltypedual(x) = eltype(x) <: ForwardDiff.Dual
 isdualtype(::Type{<:ForwardDiff.Dual}) = true
-const dualT = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag, Float64}, Float64, 1}
-dualgen(::Type{T}) where {T} = ForwardDiff.Dual{ForwardDiff.Tag{OrdinaryDiffEqTag, T}, T, 1}
 
 # Copy of the other prob2dtmin dispatch, just for optionality
 function prob2dtmin(tspan, ::ForwardDiff.Dual, use_end_time)
@@ -22,91 +25,16 @@ function prob2dtmin(tspan, ::ForwardDiff.Dual, use_end_time)
     end
 end
 
-function hasdualpromote(u0, t::Number)
-    hasmethod(ArrayInterface.promote_eltype,
-            Tuple{Type{typeof(u0)}, Type{dualgen(eltype(u0))}}) &&
-        hasmethod(promote_rule,
-            Tuple{Type{eltype(u0)}, Type{dualgen(eltype(u0))}}) &&
-        hasmethod(promote_rule,
-            Tuple{Type{eltype(u0)}, Type{typeof(t)}})
-end
+# function hasdualpromote(u0, t::Number)
+#     hasmethod(ArrayInterface.promote_eltype,
+#             Tuple{Type{typeof(u0)}, Type{dualgen(eltype(u0))}}) &&
+#         hasmethod(promote_rule,
+#             Tuple{Type{eltype(u0)}, Type{dualgen(eltype(u0))}}) &&
+#         hasmethod(promote_rule,
+#             Tuple{Type{eltype(u0)}, Type{typeof(t)}})
+# end
 
-const NORECOMPILE_IIP_SUPPORTED_ARGS = (
-    Tuple{Vector{Float64}, Vector{Float64},
-        Vector{Float64}, Float64},
-    Tuple{Vector{Float64}, Vector{Float64},
-        SciMLBase.NullParameters, Float64})
 
-const oop_arglists = (Tuple{Vector{Float64}, Vector{Float64}, Float64},
-    Tuple{Vector{Float64}, SciMLBase.NullParameters, Float64},
-    Tuple{Vector{Float64}, Vector{Float64}, dualT},
-    Tuple{Vector{dualT}, Vector{Float64}, Float64},
-    Tuple{Vector{dualT}, SciMLBase.NullParameters, Float64},
-    Tuple{Vector{Float64}, SciMLBase.NullParameters, dualT})
-
-const NORECOMPILE_OOP_SUPPORTED_ARGS = (Tuple{Vector{Float64},
-        Vector{Float64}, Float64},
-    Tuple{Vector{Float64},
-        SciMLBase.NullParameters, Float64})
-const oop_returnlists = (Vector{Float64}, Vector{Float64},
-    ntuple(x -> Vector{dualT}, length(oop_arglists) - 2)...)
-
-function wrapfun_oop(ff, inputs::Tuple = ())
-    if !isempty(inputs)
-        IT = Tuple{map(typeof, inputs)...}
-        if IT ∉ NORECOMPILE_OOP_SUPPORTED_ARGS
-            throw(NoRecompileArgumentError(IT))
-        end
-    end
-    FunctionWrappersWrappers.FunctionWrappersWrapper(ff, oop_arglists,
-        oop_returnlists)
-end
-
-function wrapfun_iip(ff,
-        inputs::Tuple{T1, T2, T3, T4}) where {T1, T2, T3, T4}
-    T = eltype(T2)
-    dualT = dualgen(T)
-    dualT1 = ArrayInterface.promote_eltype(T1, dualT)
-    dualT2 = ArrayInterface.promote_eltype(T2, dualT)
-    dualT4 = dualgen(promote_type(T, T4))
-
-    iip_arglists = (Tuple{T1, T2, T3, T4},
-        Tuple{dualT1, dualT2, T3, T4},
-        Tuple{dualT1, T2, T3, dualT4},
-        Tuple{dualT1, dualT2, T3, dualT4})
-
-    iip_returnlists = ntuple(x -> Nothing, 4)
-
-    fwt = map(iip_arglists, iip_returnlists) do A, R
-        FunctionWrappersWrappers.FunctionWrappers.FunctionWrapper{R, A}(Void(ff))
-    end
-    FunctionWrappersWrappers.FunctionWrappersWrapper{typeof(fwt), false}(fwt)
-end
-
-const iip_arglists_default = (
-    Tuple{Vector{Float64}, Vector{Float64}, Vector{Float64},
-        Float64},
-    Tuple{Vector{Float64}, Vector{Float64},
-        SciMLBase.NullParameters,
-        Float64
-    },
-    Tuple{Vector{dualT}, Vector{Float64}, Vector{Float64}, dualT},
-    Tuple{Vector{dualT}, Vector{dualT}, Vector{Float64}, dualT},
-    Tuple{Vector{dualT}, Vector{dualT}, Vector{Float64}, Float64},
-    Tuple{Vector{dualT}, Vector{dualT}, SciMLBase.NullParameters,
-        Float64
-    },
-    Tuple{Vector{dualT}, Vector{Float64},
-        SciMLBase.NullParameters, dualT
-    })
-const iip_returnlists_default = ntuple(x -> Nothing, length(iip_arglists_default))
-
-function wrapfun_iip(@nospecialize(ff))
-    fwt = map(iip_arglists_default, iip_returnlists_default) do A, R
-        FunctionWrappersWrappers.FunctionWrappers.FunctionWrapper{R, A}(Void(ff))
-    end
-    FunctionWrappersWrappers.FunctionWrappersWrapper{typeof(fwt), false}(fwt)
-end
 
 promote_dual(::Type{T}, ::Type{T2}) where {T <: ForwardDiff.Dual, T2} = T
 function promote_dual(::Type{T},
@@ -497,9 +425,9 @@ unitfulvalue(x::Type{ForwardDiff.Dual{T, V, N}}) where {T, V, N} = V
 unitfulvalue(x::ForwardDiff.Dual) = unitfulvalue(ForwardDiff.value(x))
 
 sse(x::ForwardDiff.Dual) = sse(ForwardDiff.value(x)) + sum(sse, ForwardDiff.partials(x))
-function DiffEqBase.totallength(x::ForwardDiff.Dual)
-    return DiffEqBase.totallength(ForwardDiff.value(x)) +
-           sum(DiffEqBase.totallength, ForwardDiff.partials(x))
+function SciMLBase.totallength(x::ForwardDiff.Dual)
+    return SciMLBase.totallength(ForwardDiff.value(x)) +
+           sum(SciMLBase.totallength, ForwardDiff.partials(x))
 end
 
 end
