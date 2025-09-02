411be336560c9f2bcfc0ad38500da6c0077eb153
abhro <5664668+abhro@users.noreply.github.com>
2025-09-01 22:33:47 -0400

    Fix markdown lists

--- src/ensemble/ensemble_problems.jl
+++ src/ensemble/ensemble_problems.jl
@@ -14,8 +14,8 @@ EnsembleProblem(prob::AbstractSciMLProblem;
 ## Positional Arguments
 
   - `prob`: The canonical problem of the ensemble problem. This is the prob that is seeded
-  into each `prob_func` call to be used as the one that is manipulated/changed for each
-  run of the ensemble.
+    into each `prob_func` call to be used as the one that is manipulated/changed for each
+    run of the ensemble.
 
 ## Keyword Arguments
 
@@ -103,12 +103,12 @@ Each field controls how the ensemble behaves during simulation.
 
 ## Arguments
 
-    - `prob`: The original base problem to replicate or modify.
-    - `prob_func`: A function that defines how to generate each subproblem.
-    - `output_func`: A function to post-process each individual simulation result.
-    - `reduction`: A function to combine results from all simulations.
-    - `u_init`: The initial container used to accumulate the results.
-    - `safetycopy`: Whether to copy the problem when creating subproblems (to avoid unintended modifications).
+  - `prob`: The original base problem to replicate or modify.
+  - `prob_func`: A function that defines how to generate each subproblem.
+  - `output_func`: A function to post-process each individual simulation result.
+  - `reduction`: A function to combine results from all simulations.
+  - `u_init`: The initial container used to accumulate the results.
+  - `safetycopy`: Whether to copy the problem when creating subproblems (to avoid unintended modifications).
 """
 struct EnsembleProblem{T, T2, T3, T4, T5} <: AbstractEnsembleProblem
     prob::T
